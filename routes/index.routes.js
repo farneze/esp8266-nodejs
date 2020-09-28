1a89f3ee509d9cc003371607bedaf84c15f01793
Arthur Farneze <arthurfarneze@gmail.com>
2020-09-28 17:54:11 -0300

    added a comment

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.render("index", { title: "ESP8266 🚀" }));
 
+// Write here a post request to send data to ESP8266
+// router.post("/", (req,res)=>);
 module.exports = router;
